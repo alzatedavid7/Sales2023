d0e0f82dad0ceec1bc126f4d8f5d44539b700afa
David Alzate <alzatedavid7@gmail.com>
2023-03-01 15:34:37 -0500

    begin Razor

---
+++ Sales.WEB/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using static Sales.WEB.Repositories.HttResponseWrapper;
+
+namespace Sales.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+    }
+
+}
