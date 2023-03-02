913ca7ebbfe2a46db6d0785bfcb38f87498854ba
David Alzate <alzatedavid7@gmail.com>
2023-03-01 22:46:11 -0500

    Complete Categories API controller

--- Sales.API/Controllers/CategoriesController.cs
+++ Sales.API/Controllers/CategoriesController.cs
@@ -16,19 +16,55 @@ namespace Sales.API.Controllers
             _context = context;
         }
 
-        [HttpGet]
+        [HttpGet] 
         public async Task<ActionResult> GetAsync()
         {
             return Ok(await _context.Categories.ToListAsync());
         }
 
+        [HttpGet ("{id:int}")] 
+        public async Task<ActionResult> GetAsync( int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
 
-        [HttpPost]
+            return Ok(category);
+        }
+
+
+        [HttpPost] //method for create o add
         public async Task<ActionResult> PostAsync(Category category) 
             {
             _context.Categories.Add(category); //  _context.Add(category);
             await _context.SaveChangesAsync();
             return Ok(category);
             }
+
+        [HttpPut] // method for edit
+        public async Task<ActionResult> PutAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+            return Ok(category);
+        }
+
+        [HttpDelete("{id:int}")] // method delete
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return NoContent();
+            
+        }
+
     }
 }
