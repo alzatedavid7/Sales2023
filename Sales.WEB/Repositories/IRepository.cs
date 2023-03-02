b13af8dac13a9f6dffeeed65b7ed561819386aa6
David Alzate <alzatedavid7@gmail.com>
2023-03-02 01:49:51 -0500

    changes WEB with the Button Create Category

--- Sales.WEB/Repositories/IRepository.cs
+++ Sales.WEB/Repositories/IRepository.cs
@@ -9,6 +9,13 @@ namespace Sales.WEB.Repositories
         Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
 
         Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+
+        Task<HttpResponseWrapper<object>> Delete<T>(string url);
+
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model); 
     }
+
 
 }
