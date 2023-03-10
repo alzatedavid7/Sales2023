8fa4bb199232427c0f9b9c41d3928b8d69bfc832
David Alzate <alzatedavid7@gmail.com>
2023-03-09 21:47:17 -0500

    SeedDB category - drop database and Records

---
+++ Sales.API/Data/SeedDb.cs
@@ -0,0 +1,33 @@
+﻿using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckCategoriesAsync();
+        }
+
+        private async Task CheckCategoriesAsync()
+        {
+            if (!_context.Categories.Any())
+            {
+                _context.Categories.Add(new Category { Name = "aceites" });
+                _context.Categories.Add(new Category { Name = "dulces" });
+                _context.Categories.Add(new Category { Name = "gaseosas" });
+                _context.Categories.Add(new Category { Name = "jabones" });
+                _context.Categories.Add(new Category { Name = "salsas" });
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
